b17c36409d3e17240ef32b76bdde68bf2502720f
vadi4 <vadich92@gmail.com>
2018-07-28 22:04:11 +0300

    new

---
+++ gulp/copy.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('copy', function() {
+  gulp.src([
+      'source/images/*.png',
+      'source/images/**/*.png',
+      'source/images/*.jpg',
+      'source/images/**/*.jpg',
+      'source/images/*.svg',
+      'source/images/**/*.svg',
+      '!source/images/sprite/*',
+      '!source/images/sprite/'
+  ])
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'));
+
+  gulp.src([
+      'source/fonts/*.woff'
+  ])
+    .pipe(gulp.dest('build/fonts'));
+});

68e0a3207c29fe3694c4e5be6eb4ef85710cfdd9
vadi4 <vadich92@gmail.com>
2018-11-07 17:15:11 +0300

    added script isOnScreen

--- source/scripts/client.js
+++ source/scripts/client.js
@@ -6,4 +6,35 @@ $(function() {
 
 	/////////// END JQUERY MASKED INPUT PHONE
 
+	// ISONSCRESSN START
+	
+	function isOnScreen(el) {
+		var $el = el;
+		var $window = $(window);
+		var winTop = $window.scrollTop();
+		var winHeight = $window.height();
+		var winBottom = winTop + winHeight;
+		var top, height, bottom;
+
+
+		if( $el.length ) {
+			$el.each( function() {
+				var $this = $(this);
+				top = $this.offset().top;
+				height = $this.outerHeight();
+				bottom = top + height;
+
+				if(( top >= winTop && top < winBottom ) ||
+				   ( bottom > winTop && bottom < winBottom ) ||
+				   ( height > winHeight && top <= winTop && bottom >= winBottom )) {
+
+				   	// DO SOMETHING
+
+				};
+			});
+		};
+	};	
+
+	// ISONSCREEN END
+
 });
